5bd455ca6fff1430bb475dc6cbf60c3f8c2d610d
Oliver Nybo <root@olivernybo.com>
2021-04-11 18:52:51 +0200

    ++i is a no go

--- src/database-handler/routes/api-key/newApiKey.js
+++ src/database-handler/routes/api-key/newApiKey.js
@@ -5,7 +5,7 @@ const generateKey = () => {
 	const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
 	let retVal = ''
-	for (let i = 0, n = charset.length; i < length; ++i)
+	for (let i = 0, n = charset.length; i < length; i++)
 		retVal += charset.charAt(Math.floor(Math.random() * n))
 	
 	return retVal
