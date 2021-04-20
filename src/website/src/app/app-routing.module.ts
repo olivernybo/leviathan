cbc01359e08c5735ef0f038b6865b92b1e1dda1f
Oliver Nybo <root@olivernybo.com>
2021-04-20 10:35:07 +0200

    added contact route

--- src/website/src/app/app-routing.module.ts
+++ src/website/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
