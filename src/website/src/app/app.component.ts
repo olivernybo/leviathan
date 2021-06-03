cc65737b1d1625bb1f98dc5055cffe6853bc23d7
Oliver Nybo <root@olivernybo.com>
2021-06-04 00:02:12 +0200

    particles-js on all pages

--- src/website/src/app/app.component.ts
+++ src/website/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const particlesJS: any;
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'website';
+
+	ngOnInit(): void {
+		particlesJS.load('particles-js', 'assets/particlesjs-config.json')
+	}
 }
