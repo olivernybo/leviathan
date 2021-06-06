e7abb2505f5fceed62a4f7a17a70303604c73067
Oliver Nybo <root@olivernybo.com>
2021-06-06 11:31:02 +0200

    ocd space

--- src/database-handler/routes/message/deleteMessage.js
+++ src/database-handler/routes/message/deleteMessage.js
@@ -4,7 +4,7 @@ export default (req, res) => {
 	const message = req.body.message
 	
 	if (
-		! message || typeof message !== 'object'
+		!message || typeof message !== 'object'
 		|| !message.name || !message.name.firstName || !message.name.lastName
 		|| !message.email || typeof message.email !== 'string' || !message.email.length || !message.email.includes('@')
 		|| !message.message || typeof message.message !== 'string' || !message.message.length
