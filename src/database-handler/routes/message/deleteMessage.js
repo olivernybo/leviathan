7fca28d6811f79a6cccaa80e092938284733e776
Oliver Nybo <root@olivernybo.com>
2021-06-06 11:00:18 +0200

    validation for message delete

--- src/database-handler/routes/message/deleteMessage.js
+++ src/database-handler/routes/message/deleteMessage.js
@@ -3,7 +3,16 @@ import client from '../../client.js'
 export default (req, res) => {
 	const message = req.body.message
 	
-	// todo validation
+	if (
+		! message || typeof message !== 'object'
+		|| !message.name || !message.name.firstName || !message.name.lastName
+		|| !message.email || typeof message.email !== 'string' || !message.email.length || !message.email.contains('@')
+		|| !message.message || typeof message.message !== 'string' || !message.message.length
+	) {
+		res.statusCode = 400
+		res.json({ error: 'Not a valid message' })
+		return
+	}
 	
 	client.lrem('messages', 0, JSON.stringify(message), (err, redisResponse) => {
 		if (err) {
