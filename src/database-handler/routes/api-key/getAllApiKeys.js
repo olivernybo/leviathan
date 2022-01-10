7d0cdc11392088707327ef7142759896b11246b2
Oliver Nybo <root@olivernybo.com>
2022-01-10 13:02:13 +0100

    api key comments

--- src/database-handler/routes/api-key/getAllApiKeys.js
+++ src/database-handler/routes/api-key/getAllApiKeys.js
@@ -1,5 +1,8 @@
 import client from '../../client.js'
 
+/**
+ * Gets all the API keys from the database.
+ */
 export default (_, res) => {
 	client.hgetall('api-key', (err, apiKeys) => {
 		if (err) {
--- src/database-handler/routes/api-key/newApiKey.js
+++ src/database-handler/routes/api-key/newApiKey.js
@@ -1,14 +1,21 @@
 import client from '../../client.js'
 import { generateKey } from '../../utils.js'
 
+/**
+ * Creates a new API key.
+ */
 export default (req, res) => {
+	// Get the module name from the request body
 	const name = req.body.name
+	// Get all the API keys from the database
 	client.hgetall('api-key', (err, apiKeys) => {
+		// If there is an error, return an error
 		if (err) {
 			res.statusCode = 500
 			res.json({ error: err }, true)
 			return
 		}
+		// If the module name already exists, return an error
 		for (const moduleName of Object.keys(apiKeys)) {
 			if (moduleName === name) {
 				res.statusCode = 400
@@ -17,13 +24,17 @@ export default (req, res) => {
 			}
 		}
 		
+		// Generate a new API key
 		const newKey = generateKey()
+		// Add the new API key to the database
 		client.hmset('api-key', name, newKey, (err, redisResponse) => {
+			// If there is an error, return an error
 			if (err) {
 				res.statusCode = 500
 				res.json({ error: err }, true)
 				return
 			}
+			// Otherwise, return the new API key
 			res.json({ message: redisResponse, key: newKey }, true)
 		})
 	})
