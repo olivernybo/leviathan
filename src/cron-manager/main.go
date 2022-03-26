1fa9274c6feaeae96b71b854743160a27b2114f7
Oliver Nybo <root@olivernybo.com>
2022-03-26 19:33:22 +0100

    removed testing println's

--- src/cron-manager/main.go
+++ src/cron-manager/main.go
@@ -4,12 +4,9 @@ import (
 	"cron-manager/cron"
 	"cron-manager/broadcaster"
 	"cron-manager/api"
-	"fmt"
 )
 
 func main() {
-	fmt.Println(cron.Success)
-	fmt.Println(cron.Failure)
 	// make jobs and results channels
 	jobs := make(chan cron.Job)
 	results := make(chan cron.Job)
