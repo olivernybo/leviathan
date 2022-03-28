5c51c90ec3254781c4b61df545e9bd05657ddd91
Oliver Nybo <root@olivernybo.com>
2022-03-28 08:29:04 +0200

    field validation

--- src/cron-manager/api/newJob.go
+++ src/cron-manager/api/newJob.go
@@ -9,13 +9,31 @@ import (
 
 // Route to create a new cron job
 func NewJob(w http.ResponseWriter, r *http.Request) {
+	// Get the job from the request body
+	name := r.FormValue("name")
+	command := r.FormValue("command")
+	schedule := r.FormValue("schedule")
+
+	// Validate the fields. Name and command are required. Schedule is optional. Schedule is a time format
+	if name == "" || command == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(api.Response{
+			Message: "Missing fields",
+		})
+		return
+	}
+
+	// If the schedule is empty, set it to "* * * * *" else validate the schedule format
+	if schedule == "" {
+		schedule = "* * * * *"
+	}
+	// todo validate the schedule format
 	
 	// Create a new job
-	// todo validate the request body
 	job := cron.Job{}
-	job.Name = r.FormValue("name")
-	job.Command = r.FormValue("command")
-	job.Schedule = r.FormValue("schedule")
+	job.Name = name
+	job.Command = command
+	job.Schedule = schedule
 
 	// Add the job to the jobs channel
 	JOBS <- job
